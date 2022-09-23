0edcae8c3f0fdf49704d47dc872bbc24a9973390
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-23 06:17:26 -0400

    changed deleate to a ID

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -59,7 +59,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:isbn', (req, res) => {
+router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   Category.destroy({
     where: {
